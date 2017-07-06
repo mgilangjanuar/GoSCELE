b05e79c0f8b5163bc9acce1d1c6350f0471f689a
Muhammad Gilang Januar <mjanuar@cermati.com>
2017-07-06 12:44:12 +0700

    update example instrument test

--- app/src/androidTest/java/com/mgilangjanuar/dev/goscele/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/mgilangjanuar/dev/goscele/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("mgilangjanuar.anjayyydev.com.sceleapp", appContext.getPackageName());
+        assertEquals("com.mgilangjanuar.dev.goscele", appContext.getPackageName());
     }
 }
